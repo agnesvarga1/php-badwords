2cdf241579d2b64de6acea8cb0df778affaa92a2
agnesvarga1 <agikolyok@gmail.com>
2024-02-27 13:03:32 +0100

    fix

--- otherfile.php
+++ otherfile.php
@@ -12,7 +12,8 @@ $secretWrd = $_POST["word"];
 </head>
 <body>    
     <span><?php echo str_replace($secretWrd,"***",$paragraph) ?></span>
-    <p>The length of the paragraph:<strong> <?php echo strlen($paragraph) ?></strong></p>
+    <p>The length of the original paragraph:<strong> <?php echo strlen($paragraph) ?></strong></p>
+    <p>The length of the modified paragraph:<strong> <?php echo strlen(str_replace($secretWrd,"***",$paragraph)) ?></strong></p>
     <span>The lenth of the secret word:<strong> <?php echo strlen($secretWrd) ?></strong></span>
     
 </body>
